b81c93c4e2c3a3482a4def856e2777eab29ad889
Ethan-Millán <millan.diaz.ethan@gmail.com>
2026-03-10 13:44:05 -0600

    Pequeño commit de las migraciones

---
+++ database/migrations/2026_03_10_191824_create_multas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('multas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_multa', 50);
+            $table->string('descripcion_multa', 255);
+            $table->time('hora_infraccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('multas');
+    }
+};
